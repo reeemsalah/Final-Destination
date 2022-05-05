72b6d674debc5b7faf71d8f7c58cc3ebcbbd06eb
fadyazizz <fady.aziz.1998@hotmail.com>
2022-05-05 03:12:43 +0000

    First commit

---
+++ src/main/java/com/SpotifyButHalfAsGood/App.java
@@ -0,0 +1,13 @@
+package com.SpotifyButHalfAsGood;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
