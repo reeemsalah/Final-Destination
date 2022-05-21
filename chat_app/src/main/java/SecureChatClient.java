645daed27b6a41720d9230203b5eb3dc797eb7ea
muhaaaaad <mohadsamir@hotmail.com>
2022-05-21 16:37:19 +0000

    e7na bn3ok fel chat

--- chat_app/src/main/java/SecureChatClient.java
+++ chat_app/src/main/java/SecureChatClient.java
@@ -4,12 +4,12 @@ import io.netty.channel.ChannelFuture;
 import io.netty.channel.EventLoopGroup;
 import io.netty.channel.nio.NioEventLoopGroup;
 import io.netty.channel.socket.nio.NioSocketChannel;
-//import io.netty.example.telnet.TelnetClient;
 import io.netty.handler.ssl.SslContext;
 import io.netty.handler.ssl.SslContextBuilder;
 import io.netty.handler.ssl.util.InsecureTrustManagerFactory;
 
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.Scanner;
 
@@ -19,6 +19,7 @@ public final class SecureChatClient {
     static final String HOST = System.getProperty("host", "0.0.0.0");
     static final int PORT = Integer.parseInt(System.getProperty("port", "8080"));
     private static String clientUsername;
+    private static Channel ch ;
 
     public SecureChatClient(String clientUsername) {
         this.clientUsername = clientUsername;
@@ -33,6 +34,15 @@ public final class SecureChatClient {
             System.out.println("Welcome "+ clientUsername + " :D");
         }
 
+
+
+
+    }
+
+
+    public static String execute() throws IOException, InterruptedException {
+
+
         // Configure SSL.
         final SslContext sslCtx = SslContextBuilder.forClient()
                 .trustManager(InsecureTrustManagerFactory.INSTANCE).build();
@@ -45,42 +55,58 @@ public final class SecureChatClient {
                     .handler(new SecureChatClientInitializer(sslCtx));
 
             // Start the connection attempt.
-            Channel ch = b.connect(HOST, PORT).sync().channel();
+            ch = b.connect(HOST, PORT).sync().channel();
+
 
 //            while(scanner.hasNext()){
 //                String input = scanner.nextLine();
 //                ch.writeAndFlush(
 //                        "User " + clientUsername);
 //
-//            }
-            // Read commands from the stdin.
-            ChannelFuture lastWriteFuture = null;
-            BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
-            for (;;) {
-                String line = in.readLine();
-                if (line == null) {
-                    break;
-                }
+//
 
-                // Sends the received line to the server.
-                lastWriteFuture = ch.writeAndFlush(clientUsername +": " +line + "\r\n");
 
-                // If user typed the 'bye' command, wait until the server closes
-                // the connection.
-                if ("bye".equals(line.toLowerCase())) {
-                    ch.closeFuture().sync();
-                    lastWriteFuture = ch.writeAndFlush(clientUsername +" left chat "  + "\r\n");
-                    break;
-                }
-            }
-
-            // Wait until all messages are flushed before closing the channel.
-            if (lastWriteFuture != null) {
-                lastWriteFuture.sync();
-            }
         } finally {
             // The connection is closed automatically on shutdown.
             group.shutdownGracefully();
         }
+
+        // Read commands from the stdin.
+        ChannelFuture lastWriteFuture = null;
+        BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+        for (;;) {
+            String line = in.readLine();
+            if (line == null) {
+                break;
+            }
+
+            // Sends the received line to the server.
+            lastWriteFuture = ch.writeAndFlush(clientUsername +": " +line + "\r\n");
+
+            // If user typed the 'bye' command, wait until the server closes
+            // the connection.
+            if ("bye".equals(line.toLowerCase())) {
+                ch.closeFuture().sync();
+                lastWriteFuture = ch.writeAndFlush(clientUsername +" left chat "  + "\r\n");
+                break;
+            }
+        }
+
+        // Wait until all messages are flushed before closing the channel.
+        if (lastWriteFuture != null) {
+            lastWriteFuture.sync();
+        }
+
+
+        return "";
+
     }
+
 }
+
+
+
+
+
+
+
