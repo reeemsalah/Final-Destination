88699eb3d193905b21f517c27ca0dc260ed85ccf
reeemsalah <reeem.saalah20@gmail.com>
2022-05-28 16:00:56 +0000

    Adding unit tests for recommendations app

--- musicApp/src/main/java/scalable/com/music/commands/CreatePlaylistCommand.java
+++ musicApp/src/main/java/scalable/com/music/commands/CreatePlaylistCommand.java
@@ -4,6 +4,8 @@ import scalable.com.exceptions.ValidationException;
 import scalable.com.shared.classes.Arango;
 import scalable.com.shared.classes.CommandVerifier;
 import com.arangodb.entity.BaseDocument;
+
+import java.util.ArrayList;
 import java.util.UUID;
 
 import scalable.com.shared.classes.Responder;
@@ -21,21 +23,30 @@ public class CreatePlaylistCommand extends CommandVerifier {
          try {
              UUID id = UUID.randomUUID();
             String name = body.getString("name");
+            int user_id = Integer.parseInt(this.tokenPayload.getString("id"));
+            // String user_id = body.getString("id");
             Boolean isVisible = true;
             Double rating = 0.0;
+            ArrayList<String> song_ids = new ArrayList<>();
+            int number_times_rated = 0;
+            ArrayList<Integer> people_rated = new ArrayList<>();
         
             // TODO:String UserId = authenticationParams.getString(ThreadCommand.USERNAME);
             // TODO: auto generated id             uu id
 
             arango = Arango.getInstance();
-            arango.createCollectionIfNotExists("Spotify","Playlists",false);
+            arango.createCollectionIfNotExists("spotifyArangoDb","Playlists",false);
             BaseDocument myDocument = new BaseDocument();
             //TODO : key of document
           //  myDocument.setKey(id.toString());
            myDocument.addAttribute("name",name);
             myDocument.addAttribute("isVisible", isVisible);
             myDocument.addAttribute("Rating", rating);
-           BaseDocument res = arango.createDocument("Spotify","Playlists", myDocument);
+            myDocument.addAttribute("user_id", user_id);
+            myDocument.addAttribute("song_ids", song_ids);
+            myDocument.addAttribute("number_times_rated", number_times_rated);
+            myDocument.addAttribute("people_rated",people_rated);
+           BaseDocument res = arango.createDocument("spotifyArangoDb","Playlists", myDocument);
             return Responder.makeMsgResponse("Created Playlist");
         } catch (Exception e) {
             return Responder.makeErrorResponse(e.getMessage(), 404);
@@ -50,7 +61,7 @@ public class CreatePlaylistCommand extends CommandVerifier {
 
     @Override
     public boolean isAuthNeeded() {
-        return false;
+        return true;
     }
 
     @Override
--- musicApp/src/main/java/scalable/com/music/commands/ViewFavoriteAlbumsCommand.java
+++ musicApp/src/main/java/scalable/com/music/commands/ViewFavoriteAlbumsCommand.java
@@ -1,2 +1,55 @@
-package scalable.com.music.commands;public class ViewFavoriteAlbumsCommand {
+package scalable.com.music.commands;
+
+import com.arangodb.entity.BaseDocument;
+import org.json.JSONArray;
+import org.json.JSONObject;
+import scalable.com.exceptions.ValidationException;
+import scalable.com.shared.classes.Arango;
+import scalable.com.shared.classes.CommandVerifier;
+import scalable.com.shared.classes.Responder;
+
+import java.lang.reflect.Array;
+
+public class ViewFavoriteAlbumsCommand extends CommandVerifier {
+    @Override
+    public String getCommandName() {
+        return "ViewFavoriteAlbums";
+    }
+
+    @Override
+    public String execute() {
+        Arango arango = null;
+        try {
+            String user_id = this.tokenPayload.getString("id");
+            //String w = this.uriParams.getString("user_id");
+                //changesssssss
+            arango = Arango.getInstance();
+            arango.createCollectionIfNotExists("spotifyArangoDb","FavoriteAlbums",false);
+            var a = arango.filterCollection("spotifyArangoDb", "FavoriteAlbums", "user_id", user_id);
+            JSONObject al = new JSONObject();
+            //Object[] albums = a.stream().toArray();
+//            for(int i = 0; i < albums.length; i++){
+//
+//            }
+
+            return Responder.makeMsgResponse("got so far");
+        } catch (Exception e) {
+            return Responder.makeErrorResponse(e.getMessage(), 404);
+        }
+    }
+
+    @Override
+    public String getRestAPIMethod() {
+        return "GET";
+    }
+
+    @Override
+    public boolean isAuthNeeded() {
+        return true;
+    }
+
+    @Override
+    public void validateAttributeTypes() throws ValidationException {
+
+    }
 }
