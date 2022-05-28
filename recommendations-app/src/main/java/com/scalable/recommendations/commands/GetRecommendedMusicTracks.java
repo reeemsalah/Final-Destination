589d29ec7601c7620173ded369ef682e59001246
HRagaie <hodam.ragaie@gmail.com>
2022-05-28 22:57:15 +0000

    GetRecommendedArtists Command

--- recommendations-app/src/main/java/com/scalable/recommendations/commands/GetRecommendedMusicTracks.java
+++ recommendations-app/src/main/java/com/scalable/recommendations/commands/GetRecommendedMusicTracks.java
@@ -4,10 +4,12 @@ import com.arangodb.ArangoCursor;
 import com.arangodb.entity.BaseDocument;
 import com.arangodb.velocypack.VPackSlice;
 import com.scalable.recommendations.constants.DatabaseConstants;
+import org.json.JSONObject;
 import scalable.com.exceptions.ValidationException;
 import scalable.com.shared.classes.Arango;
 import scalable.com.shared.classes.Responder;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Objects;
@@ -24,30 +26,46 @@ public class GetRecommendedMusicTracks extends RecommendationsCommand{
     public String execute() {
         System.out.println("I am executing the GetRecommendedMusicTracks command");
         //TODO get user_id from token
-        //int user_id =Integer.parseInt(this.tokenPayload.getString("id"));
-        int user_id = 1;
-        int limitno = 1;
+        //String user_id =this.tokenPayload.getString("id");
+        String user_id = "user2";
+        ArrayList<String> song_ids = new ArrayList<String>();
+        JSONObject response=new JSONObject();
+        int limitno = 3;
         Arango arango = Arango.getInstance();
-        String arango_default_id = (String) get_arango_default_id(user_id, arango);
-        System.out.println(arango_default_id);
-        if(arango_default_id != null){
-            String aqlQuery = String.format("LET similar_users = (FOR songs_history IN 1..1 OUTBOUND @user_id @@collection_name FOR user IN 1..1 INBOUND songs_history._id @@collection_name FILTER user._id != @user_id RETURN user) LET most_similar_users =(FOR similar_user IN similar_users COLLECT id = similar_user._id, name = similar_user.id, isArtist = similar_user.isArtist WITH COUNT INTO similarityCount SORT similarityCount DESC LIMIT @limitno RETURN {id,name,isArtist}  ) LET curr_user_history = (FOR songs_history IN 1..1 OUTBOUND @user_id @@collection_name RETURN TO_ARRAY(songs_history._id)) LET recommended = (FOR most_similar_user IN most_similar_users FOR song IN 1..1 OUTBOUND most_similar_user.id @@collection_name RETURN DISTINCT MINUS(FLATTEN(TO_ARRAY(song._id)),FLATTEN(curr_user_history))) FOR r in recommended FILTER length(r)>0 RETURN r");
-            HashMap<String, Object> bindVars = new HashMap<>();
-            bindVars.put("@collection_name",DatabaseConstants.MUSIC_EDGE_COLLECTION);
-            bindVars.put("limitno",limitno);
-            bindVars.put("user_id",arango_default_id);
-            ArangoCursor<List> cursor = arango.query2( DatabaseConstants.DATABASE_NAME, aqlQuery, bindVars);
-            for (List element : cursor) {
-                System.out.println(element.toString());
+        try {
+            String arango_default_id = (String) get_arango_default_id(user_id, arango);
+            System.out.println("Returned Arango Default ID: " + arango_default_id);
+            if (arango_default_id != null) {
+                String aqlQuery = String.format("LET similar_users = (FOR songs_history IN 1..1 OUTBOUND @user_id @@collection_name FOR user IN 1..1 INBOUND songs_history._id @@collection_name FILTER user._id != @user_id RETURN user) LET most_similar_users =(FOR similar_user IN similar_users COLLECT id = similar_user._id, name = similar_user.id, isArtist = similar_user.isArtist WITH COUNT INTO similarityCount SORT similarityCount DESC LIMIT @limitno RETURN {id,name,isArtist}  ) LET curr_user_history = (FOR songs_history IN 1..1 OUTBOUND @user_id @@collection_name RETURN TO_ARRAY(songs_history.id)) LET recommended = (FOR most_similar_user IN most_similar_users FOR song IN 1..1 OUTBOUND most_similar_user.id @@collection_name RETURN DISTINCT MINUS(FLATTEN(TO_ARRAY(song.id)),FLATTEN(curr_user_history))) FOR r in recommended FILTER length(r)>0 RETURN r");
+                HashMap<String, Object> bindVars = new HashMap<>();
+                bindVars.put("@collection_name", DatabaseConstants.MUSIC_EDGE_COLLECTION);
+                bindVars.put("limitno", limitno);
+                bindVars.put("user_id", arango_default_id);
+                ArangoCursor<List> cursor = arango.query2(DatabaseConstants.DATABASE_NAME, aqlQuery, bindVars);
+                for (List element : cursor) {
+                    song_ids.add((String)element.get(0));
+
+                }
+                System.out.println(song_ids.toString());
+                response.put("recommended_song_id", song_ids);
+            } else {
+                return Responder.makeMsgResponse("No User Exists");
+            }
+            if (response.isEmpty()) {
+                return Responder.makeMsgResponse("No Music Tracks to Recommend for this user");
             }
         }
+        catch (Exception e){
+            System.out.println(e.toString());
+            return  Responder.makeErrorResponse("Something went wrong",400);
+        }
+        return Responder.makeDataResponse(response);
 
-        return Responder.makeMsgResponse("Recommended Music Tracks");
 
 
     }
 
-    public Object get_arango_default_id(int user_id,Arango arango){
+    public Object get_arango_default_id(String user_id,Arango arango){
 
         try {
             HashMap<String, Object> bindVars = new HashMap<>();
@@ -56,7 +74,6 @@ public class GetRecommendedMusicTracks extends RecommendationsCommand{
             bindVars.put("userid", user_id);
             ArangoCursor<BaseDocument> cursor = arango.query(DatabaseConstants.DATABASE_NAME, aqlQuery, bindVars);
             if (!(Objects.isNull(cursor))) {
-                System.out.println("cursor not null");
                 List<BaseDocument> user = cursor.asListRemaining();
                 if (user.size() == 1) {
                     for (BaseDocument result_row : user) {
@@ -65,7 +82,7 @@ public class GetRecommendedMusicTracks extends RecommendationsCommand{
                     }
                 }
             } else {
-                Responder.makeMsgResponse("No user exists with that id");
+                return Responder.makeMsgResponse("No user exists with that id");
             }
         }
         catch (Exception e){
