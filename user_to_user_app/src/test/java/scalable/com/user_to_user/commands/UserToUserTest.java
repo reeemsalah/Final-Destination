0ea9ba43a099e768cb48968466c580a5a43a364b
RanaAMoniem <rana.a.moniem@gmail.com>
2022-05-29 11:40:35 +0000

    user to user block and follow unit tests done

--- user_to_user_app/src/test/java/scalable/com/user_to_user/commands/UserToUserTest.java
+++ user_to_user_app/src/test/java/scalable/com/user_to_user/commands/UserToUserTest.java
@@ -4,6 +4,7 @@ import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.fail;
 
 import org.json.JSONObject;
+import org.junit.AfterClass;
 import org.junit.BeforeClass;
 import org.junit.Test;
 
@@ -16,6 +17,7 @@ public class UserToUserTest {
     public static void setUp() {
         try {
             arango = Arango.getConnectedInstance();
+//            arango.dropDatabase("user_to_user");
             UserToUserApp app= new UserToUserApp();
             app.dbInit();
         } catch (Exception e) {
@@ -54,4 +56,69 @@ public class UserToUserTest {
         assertEquals(beforeCount+1, afterCount);
     }
 
+    public static String postFollowUser(int followed_id){
+
+        JSONObject body = new JSONObject();
+        body.put("followed_id", followed_id);
+
+        JSONObject uriParams = new JSONObject();
+
+        JSONObject token= new JSONObject();
+        token.put("id", "1");
+
+        JSONObject request = new JSONObject();
+        request.put("body", body);
+        request.put("methodType", "POST");
+        request.put("uriParams", uriParams);
+        request.put("isAuthenticated",true);
+        request.put("tokenPayload", token);
+
+        PostFollowUser postFollowUser = new PostFollowUser();
+        return postFollowUser.execute(request);
+    }
+
+    @Test
+    public void postFollowUserTest(){
+        int beforeCount= arango.documentCount("user_to_user", "followed_ids");
+        String response = postFollowUser(3);
+        JSONObject responseJson = new JSONObject(response);
+        int afterCount =arango.documentCount("user_to_user", "followed_ids");
+        assertEquals("Following: 3", responseJson.getString("msg"));
+        assertEquals(beforeCount+1, afterCount);
+    }
+
+    public static String postBlockUser(int blocked_id){
+
+        JSONObject body = new JSONObject();
+        body.put("blocked_id", blocked_id);
+
+        JSONObject uriParams = new JSONObject();
+
+        JSONObject token = new JSONObject();
+        token.put("id", "1");
+
+        JSONObject request = new JSONObject();
+        request.put("body", body);
+        request.put("methodType", "POST");
+        request.put("uriParams", uriParams);
+        request.put("isAuthenticated", true);
+        request.put("tokenPayload", token);
+
+        PostBlockUser postBlockUser = new PostBlockUser();
+        return postBlockUser.execute(request);
+    }
+
+    @Test
+    public void postBlockUserTest(){
+        int beforeCount= arango.documentCount("user_to_user", "blocked_ids");
+        String response = postBlockUser(4);
+        JSONObject responseJson = new JSONObject(response);
+        int afterCount =arango.documentCount("user_to_user", "blocked_ids");
+        assertEquals("Blocked: "+"4", responseJson.getString("msg"));
+        assertEquals(beforeCount+1, afterCount);
+    }
+    @AfterClass
+    public static void deleteAll(){
+        arango.dropDatabase("user_to_user");
+    }
 }
