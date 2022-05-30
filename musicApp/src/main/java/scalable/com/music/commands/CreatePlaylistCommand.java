ca99b04a0eb8bd71f8641926df4397ec5ee58d02
mahmoudAmagdy <mahmoud.magdy.mokhtar@gmail.com>
2022-05-30 16:16:03 +0000

    kaddah rebase

--- musicApp/src/main/java/scalable/com/music/commands/CreatePlaylistCommand.java
+++ musicApp/src/main/java/scalable/com/music/commands/CreatePlaylistCommand.java
@@ -4,6 +4,9 @@ import scalable.com.exceptions.ValidationException;
 import scalable.com.shared.classes.Arango;
 import scalable.com.shared.classes.CommandVerifier;
 import com.arangodb.entity.BaseDocument;
+import java.util.UUID;
+
+import scalable.com.shared.classes.Responder;
 
 
 public class CreatePlaylistCommand extends CommandVerifier {
@@ -14,10 +17,30 @@ public class CreatePlaylistCommand extends CommandVerifier {
 
     @Override
     public String execute() {
-        Arango arango = Arango.getInstance();
-        arango.createCollectionIfNotExists("Spotify","Playlists",true);
-        //BaseDocument document = new BaseDocument();
-        return null;
+        Arango arango = null;
+         try {
+             UUID id = UUID.randomUUID();
+            String name = body.getString("name");
+            Boolean isVisible = true;
+            Double rating = 0.0;
+        
+            // TODO:String UserId = authenticationParams.getString(ThreadCommand.USERNAME);
+            // TODO: auto generated id             uu id
+
+            arango = Arango.getInstance();
+            arango.createCollectionIfNotExists("Spotify","Playlists",false);
+            BaseDocument myDocument = new BaseDocument();
+            //TODO : key of document
+          //  myDocument.setKey(id.toString());
+           myDocument.addAttribute("name",name);
+            myDocument.addAttribute("isVisible", isVisible);
+            myDocument.addAttribute("Rating", rating);
+           BaseDocument res = arango.createDocument("Spotify","Playlists", myDocument);
+            return Responder.makeMsgResponse("Created Playlist");
+        } catch (Exception e) {
+            return Responder.makeErrorResponse(e.getMessage(), 404);
+        }
+
     }
 
     @Override
